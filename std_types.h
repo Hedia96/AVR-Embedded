199b6e73f88a485d86b9fda856defb040c3f4579
Hedia96 <yoyo_hedia@yahoo.com>
2019-01-11 18:08:55 +0200

    data types doesn't  depend on Compiler or Enviroment
    Data Types

---
+++ std_types.h
@@ -0,0 +1,14 @@
+#ifndef STD_TYPES
+#define STD_TYPES
+typedef unsigned char         u8;
+typedef unsigned short int   u16;
+typedef unsigned long int    u32;
+typedef signed char           s8;
+typedef signed short int     s16;
+typedef signed long int      u32;
+typedef float                f32;
+typedef double               f64;
+#endif
+/* data types is small*/*/
+
+
